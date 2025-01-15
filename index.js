ace7e34301a1a92d2366f9545ba5d8af427d5761
Sridharan2134 <sridharansridhar213@gmail.com>
2025-01-15 10:16:04 +0530

    Fixed casing issue in productController.js

--- index.js
+++ index.js
@@ -2,41 +2,33 @@ const express = require("express");
 const dotEnv = require('dotenv');
 const mongoose = require('mongoose');
 const vendorRoutes = require('./routes/vendorRoutes');
+const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
-const path = require('path');
+const path = require('path')
 
-const app = express();
+const app = express()
 
 const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
+app.use(cors())
 
-app.use(cors());
-
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected successfully!"))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error))
 
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Route to handle root URL (should be defined before other routes)
-app.use('/', (req, res) => {
-    res.send("<h1> Welcome to SELENOPHILE_SRI.03");
-});
-
-// Route handling
+app.use(bodyParser.json());
 app.use('/vendor', vendorRoutes);
-app.use('/firm', firmRoutes);
+app.use('/firm', firmRoutes)
 app.use('/product', productRoutes);
-
-// Serve static files from 'uploads' folder
 app.use('/uploads', express.static('uploads'));
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server started and running at http://localhost:${PORT}`);
+    console.log(`server started and running at ${PORT}`);
 });
+
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to SUBY");
+})
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,23 +1,17 @@
 const express = require('express');
-const path = require('path'); // Ensure this is imported for path.join()
-const productController = require(`../controllers/productController`);
+const productController = require("../controllers/productController");
 
 const router = express.Router();
 
-// Route for adding a product (with firmId)
 router.post('/add-product/:firmId', productController.addProduct);
-
-// Route for fetching products by firmId
 router.get('/:firmId/products', productController.getProductByFirm);
 
-// Route for serving uploaded images (manually served)
 router.get('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    res.header('Content-Type', 'image/jpeg'); // Adjust content-type if necessary based on the file type
-    res.sendFile(path.join(__dirname, '..', 'uploads', imageName)); // Serve image from 'uploads' directory
+    res.header('Content-Type', 'image/jpeg');
+    res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
 });
 
-// Route for deleting a product by productId
 router.delete('/:productId', productController.deleteProductById);
 
 module.exports = router;
